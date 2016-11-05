e2caac040574779f3ad51451b8d1bdbdcc554a85
Mattias Wolff <mattiaswolff@Mattiass-MacBook-Pro.local>
2016-11-05 18:32:08 +0100

    updates to status page

--- js/status.js
+++ js/status.js
@@ -1,22 +1,55 @@
-$.getJSON( "https://mf11api02.azurewebsites.net/api/deployments?sla=true", function( data ) {
+var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+];
+
+var date = new Date();
+var month = date.getMonth() + 1;
+var year = date.getFullYear();
+var currentTime = 100*year + month;
+var monthOffset = 11;
+var firstDayCurrentMonth = new Date(year, month, 1);
+var firstDayStartMonth =  new Date(new Date(firstDayCurrentMonth).setMonth(firstDayCurrentMonth.getMonth() - monthOffset));
+
+var row = new Array();
+var j = -1; 
+row[++j] = '<tr><th>Cloud</th>';
+for (var d = new Date(firstDayStartMonth); d < firstDayCurrentMonth; d.setMonth(d.getMonth() + 1)) {
+    row[++j] = '<th>';
+    row[++j] = monthNames[d.getMonth()] + '-' + d.getFullYear().toString().substr(2,2);
+    row[++j] = '</th>';    
+}
+row[++j] = '</tr>';
+$('#status_table').append(row.join(''));
+
+$.getJSON( "https://mf11api02.azurewebsites.net/api/deployments?sla=true&unit=month", function( data ) {
     
     $.each(data, function( key, deployment ) {
-        console.log(deployment.name)
-        var items = [];
-        var status = '';
-        var row='<tr><td>' + deployment.name + '</td>';
-        $.each(deployment.sla, function (key1, sla) {
-            if (sla.downtime > 0) {
-                row+='<td class="outage"></td>';
-            }
-            else {
-                row+='<td class="ok"></td>';
-            }
-            
-            
-        })
-        row+='</tr>'; 
-        $('#status_table').append(row);
+        var row = new Array();
+        var j = -1;
+        row[++j] ='<tr><td>' + deployment.name + '</td>';
+        slaValue = ''
+        for (var d = new Date(firstDayStartMonth); d < firstDayCurrentMonth; d.setMonth(d.getMonth() + 1)) {
+            yearMonth = d.getFullYear() * 100 + d.getMonth() +1;
+            status = 'none';
+            $.each(deployment.sla, function (key1, sla) {
+                if (sla.time == yearMonth) {
+                    slaValue = ((sla.uptime / (sla.uptime + sla.downtime)) * 100).toFixed(2);
+                    if (slaValue > 99.5) {
+                        status = 'great';
+                    }
+                    else if (slaValue > 99) {
+                        status = 'good';
+                    }
+                    else {
+                        status = 'outage';
+                    }
+                }
+            });
+            row[++j] ='<td class="' + status + '">' + slaValue.toString() + '</td>';
+            //row[++j] ='<td class="' + status + '"></td>';
+        }
+        row[++j] ='</tr>'; 
+        $('#status_table').append(row.join(''));
         
     });
     
